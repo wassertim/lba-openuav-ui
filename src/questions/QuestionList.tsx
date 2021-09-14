c74e7f2ce7d934e13b1a903eebc102c9cb1076ce
wassertim <tim.tkachenko@idealo.de>
2021-09-14 18:44:54 +0200

    wip

--- src/questions/QuestionList.tsx
+++ src/questions/QuestionList.tsx
@@ -4,22 +4,32 @@ import {fetchAllQuestions} from './fetch/questions';
 import {Question} from './model/question';
 
 export function QuestionList() {
-  const [questions, setQuestions] = useState([] as Question[]);
+  const [questions, setQuestions] = useState({} as Record<string, Question>);
 
   useEffect(() => {
     (async () => {
-      setQuestions(await fetchAllQuestions());
+      let questionList = await fetchAllQuestions();
+      const questionMap = questionList.reduce((qm, question) => {
+        qm[question.questionStructure] = question;
+        return {...qm, [question.questionStructure]: question};
+      }, {} as Record<string, Question>);
+      setQuestions(questionMap);
+
     })();
   }, []);
   return (
       <>
         <h1>Question List</h1>
         {
-          questions.map((question, index) => (
-              <div key={index}>
-                <Link to={`/questions/${question.id.toString()}`}>{question.questionText}</Link>
-              </div>
-          ))
+          Object.keys(questions).sort().map((questionStructure) => {
+            let question = questions[questionStructure];
+            return (
+                <div key={questionStructure} className="alert alert-info">
+                  <Link
+                      to={`/questions/${question.id.toString()}`}>{questionStructure}</Link>
+                </div>
+            );
+          })
         }
       </>
   )
